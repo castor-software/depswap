e50d39bab780863ce9d8dcdcbf235d2214837af0
nharrand <nicolas.harrand@gmail.com>
2020-10-25 15:10:08 +0100

    up

--- yasjf4j/build-jars.sh
+++ yasjf4j/build-jars.sh
@@ -10,12 +10,33 @@ if [ -d $LIB ] ; then
 fi
 mkdir $LIB
 
+
+echo " ------------------------------------------ "
+echo "dir: yasjf4j-api"
 cd yasjf4j-api
 mvn clean install -DskipTests=True > /dev/null
 cd ..
 
 
-for d in $(ls -d *)
+for d in $(ls -d yasjf4j-*)
+do
+	if [ -d $d ];
+	then
+		echo " ------------------------------------------ "
+		echo "dir: $d"
+		if [[ $d == *"yasjf4j"* ]]; then
+		  echo "To be compiled"
+			cd $d
+			mvn clean install -DskipTests=True > /dev/null
+			JAR=$(ls ./**/target/*-jar-with-dependencies.jar)
+			cp $JAR $LIB
+			cd ..
+		fi
+	fi
+done
+
+
+for d in $(ls -d *-over-yasjf4j)
 do
 	if [ -d $d ];
 	then
